1c1abc042eb2a8ff0dae87a028eacbff64d64de3
C-PLUS-PLUSandPYTHON <141658152+C-PLUS-PLUSandPYTHON@users.noreply.github.com>
2025-01-22 18:57:34 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,22 @@
-function draw_screen (image: string) {
+function draw_screen (image: number[]) {
     i = 0
     for (let index = 0; index < 25; index++) {
-        led.plotBrightness(i % 5, Math.floor(i / 5), parseFloat(image.charAt(i)) * (28 + 1 / 3))
+        led.plotBrightness(i % 5, Math.floor(i / 5), image[i])
         i += 1
     }
 }
+function _3D () {
+	
+}
+function _3D__2D__1D (x: number, y: number, z: number) {
+    return x / z * fov + y / z * fov * 5
+}
 let i = 0
-let screen = "2969289998899984999414941"
-draw_screen(screen)
+let fov = 0
+let screen: number[] = []
+let cam_x = 0
+let button_action = 0
+fov = 1
+basic.forever(function () {
+	
+})
