293a34699836fc87c6324db81edca61c4e51ab79
C-PLUS-PLUSandPYTHON <141658152+C-PLUS-PLUSandPYTHON@users.noreply.github.com>
2025-01-22 19:15:11 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -10,23 +10,28 @@ function _3D () {
     for (let index = 0; index < 25; index++) {
         screen.push(0)
     }
+    screen[_3D__2D__1D(0, 0, 2)] = 128
+    screen[_3D__2D__1D(4, 0, 2)] = 128
+    screen[_3D__2D__1D(4, 4, 2)] = 128
+    screen[_3D__2D__1D(0, 4, 2)] = 128
     screen[_3D__2D__1D(0, 0, 1)] = 255
     screen[_3D__2D__1D(4, 0, 1)] = 255
     screen[_3D__2D__1D(4, 4, 1)] = 255
     screen[_3D__2D__1D(0, 4, 1)] = 255
-    screen[_3D__2D__1D(0, 0, 2)] = 255
-    screen[_3D__2D__1D(4, 0, 2)] = 255
-    screen[_3D__2D__1D(4, 4, 2)] = 255
-    screen[_3D__2D__1D(0, 4, 2)] = 255
 }
 function _3D__2D__1D (x: number, y: number, z: number) {
-    return x / z * fov + y / z * fov * 5
+    return Math.floor((x - cam_x) / (z - cam_z) * fov) + Math.floor((y - cam_y) / (z - cam_z) * fov * 5)
 }
 let i = 0
 let fov = 0
+let cam_z = 0
+let cam_y = 0
+let cam_x = 0
 let screen: number[] = []
 screen = []
-let cam_x = 0
+cam_x = 0
+cam_y = 0
+cam_z = 0
 let button_action = 0
 fov = 1
 basic.forever(function () {
